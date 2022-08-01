07ea05012010b00212144a0448db2325cbb5dc00
yuannan <xinyet@aliyun.com>
2022-08-01 16:58:47 +0800

    fix quartz

---
+++ quartz/src/main/java/com/xinyet/quartz/QuartzApp.java
@@ -0,0 +1,11 @@
+package com.xinyet.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzApp {
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzApp.class, args);
+    }
+}
